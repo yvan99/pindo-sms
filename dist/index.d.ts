a3683105be4e25cf50d74d85726d8156cb63863b
yvan99 <ishimweyvan90@gmail.com>
2024-07-19 23:20:47 +0200

    🤡 🍻 Damn it , i pushed node_modules

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { BulkSMSPayload, SMSPayload } from './types/interfaces/PindoInterface.interface';
-declare class PindoSMS {
-    private apiUrl;
-    private token;
-    constructor(token: string);
-    sendSMS(payload: SMSPayload): Promise<any>;
-    sendBulkSMS(payload: BulkSMSPayload): Promise<any>;
-}
-export { PindoSMS, SMSPayload, BulkSMSPayload };
